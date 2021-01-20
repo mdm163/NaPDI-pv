b45d58eee968bc73472e6b30a3d1c87cd0a47900
Sanya <sbt12@pitt.edu>
2021-01-20 12:48:50 +0530

    update script

--- LNHPD/get_data_lnhpd.py
+++ LNHPD/get_data_lnhpd.py
@@ -1,8 +1,12 @@
-import requests
-import json, os
+#1. (python script) please make the URI a parameter to the script since it will likely change
 
-#dir_out = '/home/sanya/npdi-workspace/LNHPD/LNHPD_output/'
-dir_out = "/Users/sanya/NaPDI-pv/LNHPD/LNHPD_output"
+#5. In the pentaho workflow, I would set the path to the python script and to any data files to be relative to the job to make it more portable. 
+#Also, you need some kind of error checking so that if the python script fails the job stops. I can't see this in Pentaho right now so am not sure if you have that. 
+
+import requests
+import json, os, sys
+
+dir_out = sys.argv[1]
 
 def call_API(link):
 	response = requests.get(link)
@@ -17,12 +21,11 @@ def extract_data_from_api(page_start, page_end):
 	lnhpd = []
 	count = 0
 	total_count = 0
-	file_p = open(dir_out+'lnhpd_page_completed.txt', 'a')
+	file_p = open(dir_out+'lnhpd_page_completed.txt', 'w')
 
 	#Total pages = 6292 (as of 2020-01-07)
 	for page_no in range(page_start, page_end+1):
 		#returns paginated response with 100 objects per page
-		#Handle - duplicate data present in source files
 		uri = "https://health-products.canada.ca/api/natural-licences/medicinalingredient/?page="+str(page_no)+"&lang=en&type=json"
 		response = call_API(uri)
 		result = response.json()
@@ -50,9 +53,8 @@ def extract_data_from_api(page_start, page_end):
 	return total_count
 
 def remove_duplicates_and_save(total_count):
-	print('De-duplicating all objects')
-	lnhpd_new_all = []
-	lnhpd_ids_list = []
+	print("De-duplicating all objects")
+	lnhpd_new_all = {}
 	files = os.listdir(dir_out)
 	for file in files:
 		if file[-5:] == '.json':
@@ -60,17 +62,19 @@ def remove_duplicates_and_save(total_count):
 			print('\nFilename: ', file)
 			lnhpd = json.load(file_i)
 			for item in lnhpd:
-				if item['lnhpd_id'] not in lnhpd_ids_list:
-					lnhpd_new_all.append(item)
-					lnhpd_ids_list.append(item['lnhpd_id'])
-
+				lnhpd_id = item['lnhpd_id']
+				if lnhpd_id not in lnhpd_new_all:
+					lnhpd_new_all[lnhpd_id] = item
+	lnhpd_all_unique = []
+	for key in lnhpd_new_all:
+		lnhpd_all_unique.append(lnhpd_new_all[key])
 	outfile = dir_out + 'lnhpd_all_unique.json'
-	new_count = len(lnhpd_new_all)
-	toJSON(outfile, lnhpd_new_all)
+	new_count = len(lnhpd_all_unique)
+	toJSON(outfile, lnhpd_all_unique)
 	return new_count
 
 if __name__ == '__main__':
-	total_count = extract_data_from_api(1, 7000)
+	total_count = extract_data_from_api(1, 100000)
 	print('Total objects saved: ', total_count)
 	total_count_new = remove_duplicates_and_save(total_count)
 	print('Total object after de-duplication: ', total_count_new)
